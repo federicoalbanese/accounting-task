1095b07aac6262d6f7b90ebac7c7cde59b8f36c1
alireza <a.rouhani@dinadev.ir>
2023-01-04 00:34:12 +0330

    create resource for AccessTokenResource

---
+++ app/Http/Resources/V1/AccessTokenResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AccessTokenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request): array|\JsonSerializable|\Illuminate\Contracts\Support\Arrayable
+    {
+        return [
+            'access_token' => $this->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $this->token->expires_at
+            )->toDateTimeString(),
+        ];
+    }
+}
