d386f542b6aabb1dc95a9f457514061468675572
alireza <a.rouhani@dinadev.ir>
2023-01-04 00:27:28 +0330

    create model for role and permission

---
+++ app/Models/Permission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Permission
+ *
+ * @package App\Models
+ * @property integer   id
+ * @property string    uuid
+ * @property string    title
+ * @property string    action
+ * @property \DateTime created_at
+ * @property \DateTime updated_at
+ */
+class Permission extends Model
+{
+    /**
+     * The roles that belong to the permission.
+     *
+     * @return BelongsToMany
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
