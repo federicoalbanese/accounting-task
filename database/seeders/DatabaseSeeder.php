6e791b706345723417666e30d128e5d50fd7a7ba
alireza <a.rouhani@dinadev.ir>
2023-01-04 15:24:35 +0330

    add base data seeder for test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Constants\RoleConstants;
+use App\Models\CustomerDocument;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(3)->create();
+        \App\Models\User::factory(3)->create();
 
+        for ($i = 0; $i < 5; $i++) {
+            $cd = new CustomerDocument();
+            $cd->setName('test' . $i);
+            $cd->save();
+        }
 
         foreach (RoleConstants::ROLES as $role) {
             $roleObj = new Role();
