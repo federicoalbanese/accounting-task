29bcdf6d8e112573e8f5b6df702141d499b93eb1
alireza <a.rouhani@dinadev.ir>
2023-01-04 12:45:51 +0330

    create base DocumentController

---
+++ app/Http/Controllers/Api/V1/DocumentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Api\ApiController;
+
+class DocumentController extends ApiController
+{
+    public function indexَAction()
+    {
+
+    }
+}
