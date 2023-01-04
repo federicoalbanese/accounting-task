d16eaafa3c170261590d2f47bf928d84985ed348
alireza <a.rouhani@dinadev.ir>
2023-01-04 17:36:53 +0330

    add makeConfirmDocument in ReviewerDocumentController

--- app/Http/Controllers/Api/V1/ReviewerDocumentController.php
+++ app/Http/Controllers/Api/V1/ReviewerDocumentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Constants\DocumentConstant;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Resources\V1\DocumentResource;
+use App\Models\Document;
 use App\Services\DocumentService;
+use Illuminate\Http\JsonResponse;
 
 class ReviewerDocumentController extends ApiController
 {
@@ -20,5 +23,39 @@ class ReviewerDocumentController extends ApiController
         $documents = $this->documentService->getDocumentList($this->getUser()->id);
 
         return $this->success(DocumentResource::collection($documents));
+    }
+
+    /**
+     * @param Document $document
+     *
+     * @return JsonResponse
+     */
+    public function makeConfirmDocument(Document $document)
+    {
+        $user = $this->getUser();
+        if ($this->documentNotConfirmed($document, $user->id)) {
+            $this->documentService->confirmDocument($document, $user->id);
+
+            return $this->success(
+                [
+                    'message' => __('document confirm successfully.'),
+                ]
+            );
+        }
+
+        return $this->error(
+            [
+                'message' => __('document already confirmed.'),
+            ]
+        );
+    }
+
+    private function documentNotConfirmed(Document $document, int $userId): bool
+    {
+        if (is_null($document->getAssignedTo()) && $document->getStatus() === DocumentConstant::STATUS_INIT) {
+            return true;
+        }
+
+        return false;
     }
 }
