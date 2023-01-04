1ec36474f9a2ffc2d0f51fd46a7e2e982e2209bc
alireza <a.rouhani@dinadev.ir>
2023-01-04 21:30:26 +0330

    create Middleware to check user role and perimssions

---
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Constants\RoleConstants;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class PermissionMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return RedirectResponse|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var User $user */
+        if ($user = $request->user()) {
+            // super admin is allowed to do any action
+            if ($user->hasRole(RoleConstants::SUPER_ADMIN)) {
+                return $next($request);
+            }
+            if ($user->hasPermissions($request->route()->getActionName())) {
+                return $next($request);
+            }
+
+            abort(403);
+        }
+
+        return redirect()->route('login');
+    }
+}
