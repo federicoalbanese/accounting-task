1e092244416e2b5120e011a5dc4f2a84f7ecc08b
alireza <a.rouhani@dinadev.ir>
2023-01-04 16:07:21 +0330

    add new method to pick and store documents

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -2,7 +2,52 @@
 
 namespace App\Services;
 
+use App\Constants\DocumentConstant;
+use App\Models\CustomerDocument;
+use App\Models\Document;
+use Illuminate\Database\Eloquent\Collection;
+
 class DocumentService
 {
+    /**
+     * @return Collection|array
+     */
+    public function getDocumentList(): Collection|array
+    {
+        return CustomerDocument::query()
+            ->where('status', '=', DocumentConstant::STATUS_INIT)
+            ->whereNull('assigned_to')
+            ->get();
+    }
 
+    /**
+     * @param CustomerDocument $customerDocument
+     * @param int              $userId
+     *
+     * @return CustomerDocument
+     */
+    public function pick(CustomerDocument $customerDocument, int $userId): CustomerDocument
+    {
+        $customerDocument->setAssignedTo($userId);
+        $customerDocument->save();
+
+        return $customerDocument->refresh();
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return Document
+     */
+    public function store(array $parameters): Document
+    {
+        $document = new Document();
+
+        $document->setName($parameters['name']);
+        $document->setCreatedBy($parameters['user_id']);
+        $document->setPriority($parameters['priority']);
+        $document->save();
+
+        return $document->refresh();
+    }
 }
